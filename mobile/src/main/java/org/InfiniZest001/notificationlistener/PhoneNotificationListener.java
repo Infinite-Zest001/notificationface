2317309edc2ae18290117928083351cdd2a6a774
Imran <imran.khan2373@gmail.com>
2018-06-19 12:47:00 -0400

    CommitVer M

--- mobile/src/main/java/org/InfiniZest001/notificationlistener/PhoneNotificationListener.java
+++ mobile/src/main/java/org/InfiniZest001/notificationlistener/PhoneNotificationListener.java
@@ -1,19 +1,22 @@
 package org.InfiniZest001.notificationlistener;
 
-import android.app.Notification;
 import android.app.NotificationManager;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
-
-import org.jfedor.notificationface.NotificationListener;
 
 import java.io.ByteArrayOutputStream;
 import java.util.*;
+
+import kotlin.collections.CollectionsKt;
+import kotlin.collections.IndexedValue;
+
 
 public class PhoneNotificationListener extends NotificationListenerService {
     private static final int ICON_SIZE = 48;
@@ -22,17 +25,17 @@ public class PhoneNotificationListener extends NotificationListenerService {
     @Override
     public void onListenerConnected() {
         super.onListenerConnected();
-        sendToWatch(currentRanking);
+        this.sendToWatch(this.getCurrentRanking());
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn, RankingMap rankingMap) {
-        sendToWatch(rankingMap);
+        this.sendToWatch(rankingMap);
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn, RankingMap rankingMap) {
-        sendToWatch(rankingMap);
+        this.sendToWatch(rankingMap);
     }
 
     private void sendToWatch(RankingMap rankingMap) {
@@ -50,20 +53,19 @@ public class PhoneNotificationListener extends NotificationListenerService {
 
             Bitmap bitmap = this.drawableToBitmap(
                     notification.getNotification().getSmallIcon().loadDrawable(this));
-            //Extracted from Kotlin decompile
-            Iterable receiver = (Iterable)bitmaps;
+            Iterable itX = (Iterable)bitmaps;
             boolean x;
-            if ( receiver instanceof Collection && ((Collection)receiver).isEmpty()) {
+            if (itX instanceof Collection  && ((Collection)itX).isEmpty()) {
                 x = false;
             } else {
-                Iterator y = receiver.iterator();
+                Iterator itY = bitmaps.iterator();
                 while(true) {
-                    if (!y.hasNext()) {
+                    if (!itY.hasNext()) {
                         x = false;
                         break;
                     }
 
-                    Object e = y.next();
+                    Object e = itY.next();
                     Bitmap it = (Bitmap)e;
                     if (it.sameAs(bitmap)) {
                         x = true;
@@ -76,10 +78,22 @@ public class PhoneNotificationListener extends NotificationListenerService {
                 bitmaps.add(bitmap);
             }
         }
-        //TODO implement the for in loop
+        Iterator var18 = CollectionsKt.withIndex((Iterable)bitmaps).iterator();
+
+        while(var18.hasNext()) {
+            IndexedValue var16 = (IndexedValue)var18.next();
+            int i = var16.component1();
+            Bitmap bitmap = (Bitmap)var16.component2();
+            putDataMapReq.getDataMap().putByteArray("icon" + i, this.bitmapToByteArray(bitmap));
+            putDataMapReq.getDataMap().putByteArray("safeicon" + i, this.bitmapToByteArray(this.createBurnInSafeBitmap(bitmap)));
+        }
+
+        putDataMapReq.setUrgent();
+        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+        Wearable.getDataClient((Context)this).putDataItem(putDataReq);
     }
 
-    private final Bitmap drawableToBitmap(Drawable drawable) {
+    private Bitmap drawableToBitmap(Drawable drawable) {
         Bitmap bitmap = Bitmap.createBitmap(ICON_SIZE, ICON_SIZE, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -87,6 +101,26 @@ public class PhoneNotificationListener extends NotificationListenerService {
         return bitmap;
     }
 
-    //TODO: Implement bitmapToByteArray
-    //TODO: Implement  createBurnInSafeBitmap
+    private byte[] bitmapToByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        return  stream.toByteArray();
+    }
+
+    private Bitmap createBurnInSafeBitmap(Bitmap bitmap) {
+        Bitmap bitmap2 = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        int x = 0;
+
+        for(int var4 = bitmap.getWidth(); x < var4; ++x) {
+            int y = 0;
+
+            for(int var6 = bitmap.getHeight(); y < var6; ++y) {
+                if (y % 4 == 2 * (x % 2)) {
+                    bitmap2.setPixel(x, y, bitmap.getPixel(x, y));
+                }
+            }
+        }
+
+        return  bitmap2;
+    }
 }
